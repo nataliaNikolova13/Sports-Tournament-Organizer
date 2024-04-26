d3b1931d274557d47c4da74f0e638a70c55a9a8b
Natalia Nikolova <104618909+nataliaNikolova13@users.noreply.github.com>
2024-04-26 19:11:02 +0300

    empty project and connection to database

---
+++ sport-tournament/src/main/java/com/fmi/sporttournament/SportTournamentApplication.java
@@ -0,0 +1,13 @@
+package com.fmi.sporttournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportTournamentApplication.class, args);
+	}
+
+}
---
+++ sport-tournament/src/test/java/com/fmi/sporttournament/SportTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmi.sporttournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportTournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
