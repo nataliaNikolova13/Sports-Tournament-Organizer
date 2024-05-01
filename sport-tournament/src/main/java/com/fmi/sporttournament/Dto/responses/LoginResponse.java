8ce690f2e1741cd56c518c220eed516ea741ab7b
Natalia Nikolova <104618909+nataliaNikolova13@users.noreply.github.com>
2024-05-01 17:58:19 +0300

    jwt authentication
    mostly working; rework roles

---
+++ sport-tournament/src/main/java/com/fmi/sporttournament/Dto/responses/LoginResponse.java
@@ -0,0 +1,16 @@
+package com.fmi.sporttournament.Dto.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginResponse {
+    private String token;
+
+    private long expiresIn;
+}
--- sport-tournament/src/main/java/com/fmi/sporttournament/entity/User.java
+++ sport-tournament/src/main/java/com/fmi/sporttournament/entity/User.java
@@ -3,19 +3,25 @@ package com.fmi.sporttournament.entity;
 import com.fmi.sporttournament.entity.enums.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Table(name = "users")
 @Entity
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(nullable = false)
@@ -41,4 +47,34 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(name="role")
     private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
