70659a28c6e7bbb6f97539c1535737798c910c4c
yoanapeeva <yoana_peeva@abv.bg>
2024-05-18 22:07:29 +0300

    Add AutoIncrement Venue Id

--- sport-tournament/src/main/java/com/fmi/sporttournament/entity/Venue.java
+++ sport-tournament/src/main/java/com/fmi/sporttournament/entity/Venue.java
@@ -3,6 +3,8 @@ package com.fmi.sporttournament.entity;
 import jakarta.persistence.CascadeType;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -25,6 +27,7 @@ import java.util.Set;
 public class Venue {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
