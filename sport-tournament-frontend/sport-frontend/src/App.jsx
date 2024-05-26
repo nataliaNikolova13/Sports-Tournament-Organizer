1c8de007456d4361c299eeafdcd7babc03fa96b9
Natalia Nikolova <104618909+nataliaNikolova13@users.noreply.github.com>
2024-05-26 21:53:04 +0300

    login + router

--- sport-tournament-frontend/sport-frontend/src/App.jsx
+++ sport-tournament-frontend/sport-frontend/src/App.jsx
@@ -3,12 +3,20 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import RegistrationForm from "./forms/registration/Registration";
+import LoginForm from "./forms/login/Login";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <RegistrationForm></RegistrationForm>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginForm></LoginForm>}></Route>
+        <Route
+          path="/registration"
+          element={<RegistrationForm></RegistrationForm>}
+        ></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ sport-tournament-frontend/sport-frontend/src/forms/login/Login.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Login.css";
+import { Link } from "react-router-dom";
+
+const LoginForm = () => {
+  const initialFormState = {
+    email: "",
+    password: "",
+  };
+  const [formData, setFormData] = useState(initialFormState);
+
+  const [errors, setErrors] = useState({});
+  const [loginError, setLoginError] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const validate = () => {
+    const errors = {};
+    if (!formData.email) {
+      errors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      errors.email = "Email address is invalid";
+    }
+    if (!formData.password) errors.password = "Password is required";
+    return errors;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const validationErrors = validate();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+    } else {
+      try {
+        const response = await axios.post(
+          "http://localhost:8080/auth/login",
+          formData
+        );
+        console.log("Form Data Submitted", response.data);
+        localStorage.setItem("token", response.data.token);
+        clear();
+      } catch (error) {
+        setLoginError("No such user");
+      }
+    }
+  };
+
+  const clear = () => {
+    setFormData(initialFormState);
+    setErrors({});
+    setLoginError("");
+  };
+
+  return (
+    <div className="container">
+      <h2>Login Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          {errors.email && <p className="error">{errors.email}</p>}
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          {errors.password && <p className="error">{errors.password}</p>}
+        </div>
+        <button type="submit">Login</button>
+        {loginError && <p className="error">{loginError}</p>}
+        <Link to="/registration">Register</Link>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
